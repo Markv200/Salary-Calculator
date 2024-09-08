9903f6a124759df112dbc52e6a00e49d45bb439d
Mark Velesaca <mark.velesaca@gmail.com>
2024-09-08 03:16:07 -0500

    Buttin

--- myscript.js
+++ myscript.js
@@ -1,9 +1,16 @@
 console.log('sfhdfsbjh')
 
 
-function add(){
-    
+function submitForm(event){
+    event.preventDefault()
 
+    let inputfirstName = document.querySelector('[data-testid="firstNameInput"]').value
+    let inputlastName =  document.querySelector('[data-testid="lastNameInput"]').value
+    let inputID = document.querySelector('[data-testid="idInput"]').value
+    let inputtitle = document.querySelector('[data-testid="titleInput"]').value
+    let inputSalary = document.querySelector('[data-testid="annualSalaryInput"]').value
+
+    
 }
 
 
